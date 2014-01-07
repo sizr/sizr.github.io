90d0d30e523a354c3076dcf0907659845253d281
Kartik Luke Singh <kartikluke@gmail.com>
2014-01-07 18:53:02 +0530

    Site updated at 2014-01-07 13:23:02 UTC

---
+++ javascripts/scripts.js
@@ -0,0 +1,48 @@
+(function ($, window, document, undefined) {
+
+  'use strict';
+
+  $(function () {
+    // FireShell
+
+		function Midway(){
+	        var $centerHorizontal = $('.midway-horizontal'),
+	            $centerVertical = $('.midway-vertical');
+
+	        $centerHorizontal.each(function(){
+	            $(this).css('marginLeft', -$(this).outerWidth()/2);
+	          });
+	        $centerVertical.each(function(){
+	            $(this).css('marginTop', -$(this).outerHeight()/2);
+	          });
+	        $centerHorizontal.css({
+	            'display' : 'inline',
+	            'position' : 'absolute',
+	            'left' : '50%'
+	          });
+	        $centerVertical.css({
+	            'display' : 'inline',
+	            'position' : 'absolute',
+	            'top' : '50%',
+	          });
+		    }
+		$(window).on('load', Midway);
+		$(window).on('resize', Midway);
+		$(window).on('load', Midway);
+
+		$('a[href^="#"]').on('click',function (e) {
+		    e.preventDefault();
+
+		    var target = this.hash,
+		    $target = $(target);
+
+		    $('html, body').stop().animate({
+		        'scrollTop': $target.offset().top
+		    }, 900, 'easeInSine', function () {
+		        window.location.hash = target;
+		    });
+		});
+
+  });
+
+})(jQuery, window, document);
