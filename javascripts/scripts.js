29bc0315c9a4711ba966bddd2ddcd2991f8d39a5
Kartik Luke Singh <kartikluke@gmail.com>
2014-07-17 14:17:18 +0530

    Site updated at 2014-07-17 08:47:18 UTC

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -45,18 +45,18 @@
 
 		var month = [];
 
-		month[0] = "January";
-		month[1] = "February";
-		month[2] = "March";
-		month[3] = "April";
-		month[4] = "May";
-		month[5] = "June";
-		month[6] = "July";
-		month[7] = "August";
-		month[8] = "September";
-		month[9] = "October";
-		month[10] = "November";
-		month[11] = "December";
+		month[0] = " January";
+		month[1] = " February";
+		month[2] = " March";
+		month[3] = " April";
+		month[4] = " May";
+		month[5] = " June";
+		month[6] = " July";
+		month[7] = " August";
+		month[8] = " September";
+		month[9] = " October";
+		month[10] = " November";
+		month[11] = " December";
 
 		function setDates() {
 			var d = new Date(),
